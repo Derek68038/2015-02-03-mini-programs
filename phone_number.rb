1af810a9fb618b2ebbc84321f35e850329599623
Derek George <derek68038@gmail.com>
2015-02-03 19:16:07 -0600

    Implemented the ActiveSupport methods and provided documentation.

--- phone_number.rb
+++ phone_number.rb
@@ -1,12 +1,54 @@
 
+require 'active_support'
+require 'active_support/all'
+
+# Class: Number
+#
+# Takes in a string of digits as argument and outputs it in the format of a phone number.
+#
+# Attributes:
+# None
+#
+# Public Methods:
+# #driver
+
 class Number
   
-  def convert_to_phone(digits)
-    digits = digits.byteslice(0, 3) + "-" + digits.byteslice(3, 3) + "-" + digits.byteslice(6, 4)
+  include ActiveSupport::NumberHelper
+  
+  # This is my original code:
+  # def convert_to_phone(digits)
+  #   if digits.length == 10
+  #     digits = digits.byteslice(0, 3) + "-" + digits.byteslice(3, 3) + "-" + digits.byteslice(6, 4)
+  #   elsif digits.length == 11
+  #     digits = digits.byteslice(0, 1) + "-" + digits.byteslice(1, 3) + "-" + digits.byteslice(3, 3) + "-" + digits.byteslice(7, 4)
+  #   else
+  #     puts "Your number was entered incorrectly."
+  #   end
+  # end
+  
+  def number_to_phone(number, options = {})
+    NumberToPhoneConverter.convert(number, options)
   end
 
 end
 
-num = Number.new
-puts num.convert_to_phone("1234567890")
+  # Public: #driver
+  # Creates a class object then calls the number_to_phone method with a string of digits as argument.
+  #
+  # Parameters:
+  # None
+  #
+  # Returns:
+  # The argument in a phone number format.
+  #
+  # State Changes:
+  # None
+  
+  def driver
+    num = Number.new
+    puts num.number_to_phone("1234567890")
+  end
+
+ driver
 
